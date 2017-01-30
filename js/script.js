649c987e6ba7251dfdd436596d92d4e798cf30fe
Nikhil Motiani <nikhil.m@247-inc.com>
2017-01-31 01:46:57 +0530

    Fixed Draft Data

--- js/script.js
+++ js/script.js
@@ -1,170 +1,214 @@
-﻿$(document).ready(function() {
-	"use strict";
+﻿(function ($) {
+  $(document).ready(function () {
+    "use strict";
 
-	/*RSVP Form*/
-	$(".submit_block_1").on("click", function(e){
-		send_form('block_1');
-		return false;
-	});
-	
-	function send_form(type){
-	
-	var name = $("input#name_"+type).val();
-	if (name == "") {
-		$("input#name_"+type).css({border:"1px solid red"});
-		$("input#name_"+type).focus();
-		return false;
-	}
-	var email = $("input#email_"+type).val();
-	if (email == "") {
-		$("input#email_"+type).css({border:"1px solid red"});
-		$("input#email_"+type).focus();
-		return false;
-	}
-	var guest = $("input#guest_"+type).val();
-	if (guest == "") {
-		$("input#guest_"+type).css({border:"1px solid red"});
-		$("input#guest_"+type).focus();
-		return false;
-	}
-	var attending = $("input#attending_"+type).val();
-	if (attending == "") {
-		$("input#attending_"+type).css({border:"1px solid red"});
-		$("input#attending_"+type).focus();
-		return false;
-	}
-	
-	var dataString = '&name=' + name + '&email=' + email + '&guest=' + guest + '&attending=' + attending;
-	var form = $(this);
-	var str = form.serialize(); 
-	$.ajax({
-		type: "POST", url: "mail/mail.php",	data: dataString, success: function() {
-		$('#div_'+type).html("<div id='form_send_message'>Thank you :)</div>", 1500);
-	}
-	});
-	}
+    /*RSVP Form*/
+    $(".submit_block_1").on("click", function (e) {
+      send_form('block_1');
+      return false;
+    });
 
-	/*ScrollR */
-	if ($(window).width() > 1024) {
-	var s = skrollr.init({
-		forceHeight: false
-	});
-	}
+    function send_form (type) {
 
-	/*Gallery ColorBox */
-	$('.gallery_txt a').colorbox({
-		rel:'gal',
-		maxWidth:"100%",
-	});
+      var name = $("input#name_" + type).val();
+      if (name == "") {
+        $("input#name_" + type).css({border: "1px solid red"});
+        $("input#name_" + type).focus();
+        return false;
+      }
+      var email = $("input#email_" + type).val();
+      if (email == "") {
+        $("input#email_" + type).css({border: "1px solid red"});
+        $("input#email_" + type).focus();
+        return false;
+      }
+      var guest = $("input#guest_" + type).val();
+      if (guest == "") {
+        $("input#guest_" + type).css({border: "1px solid red"});
+        $("input#guest_" + type).focus();
+        return false;
+      }
+      var attending = $("input#attending_" + type).val();
+      if (attending == "") {
+        $("input#attending_" + type).css({border: "1px solid red"});
+        $("input#attending_" + type).focus();
+        return false;
+      }
 
-	/*Main Menu Button */
-	$('.main_menu_btn').on("click", function(e){
-		$(this).toggleClass('main_menu_btn_open');
-		$('.main_menu_block').toggleClass('main_menu_block_open').fadeToggle();
-		$('.main_menu_block').find('.menu_wrapper').toggleClass('active');
-		$('header .anim').toggleClass('active');
-		e.preventDefault();
-	});
-	
-	/* Section Background */
-	$('section, .parallax').each(function(){
-		var image = $(this).attr('data-image');
-		if (image){
-			$(this).css('background-image', 'url('+image+')');	
-		}
-	});
-	
-	/*ColorBox*/
-	if ($(window).width() >= 760) {
-		$(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
-	}else{
-		$(".youtube").colorbox({iframe:true, innerWidth:320, innerHeight:240});
-	}
-	$(window).resize(function () {
-		if ($(window).width() >= 760) {
-			$(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
-		}else{
-			$(".youtube").colorbox({iframe:true, innerWidth:320, innerHeight:240});
-		}
-	 });
-	
-	/*Scroll Effect*/
-	$('.intro_down, .go').on("click", function(e){
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top
-		}, 1000);
-		e.preventDefault();
-	});
-	
-	/*Show/Hide Photo in When&Where Block*/
-	$('.photocamera span').on("click", function(e){
-		$(this).parents('section').find('.opacity').toggleClass('fade');
-		$(this).parents('section').find('.over').fadeToggle();
-		e.preventDefault();
-	});
+      var dataString = '&entry.1507026591=' + name + '&entry.39623412=' + email + '&entry.475560411=' + guest + '&entry.1161442756=' + attending;
+      var form = $(this);
+      var str = form.serialize();
+      function sent(){
+        $('#div_' + type).html("<div id='form_send_message'>Thank you :)</div>", 1500);
+      }
+      $.ajax({
+        type: "POST",
+        url: "https://docs.google.com/forms/d/e/1FAIpQLSf1d8CJr38panC2YqtCgVsmX2TAaFwZCTAGuGPCzuFXMz2UCw/formResponse",
+        data: dataString,
+        complete:sent
+      });
+    }
 
-	/*Player*/
-	$('.music').on("click", function(e){
-		$('.player').fadeToggle();
-		e.preventDefault();
-	});
+    /*ScrollR */
+    if ($(window).width() > 1024) {
+      var s = skrollr.init({
+        forceHeight: false
+      });
+    }
 
-	
-	/*CountDown*/
-	$('.married_coundown').countdown({until: new Date("March 1, 2017 11:00:00")});
-	
-	/*OWL Carousel in Our Story*/
-	$(".story_wrapper").owlCarousel({
- 		navigation : true,	responsive: true, responsiveRefreshRate : 200,	slideSpeed : 200,	
- 		paginationSpeed : 200,	rewindSpeed : 500,	items:3,  itemsTablet: [768,1], autoPlay : true,
-		itemsMobile : [479,1], 	itemsDesktopSmall : [980,1],  itemsDesktop : [1500,2], mouseDrag:false
-	});
+    /*Gallery ColorBox */
+    $('.gallery_txt a').colorbox({
+      rel: 'gal',
+      maxWidth: "100%",
+    });
 
-	/*Gallery Carousel */
-	$(".gallery_wrapper").owlCarousel({
- 		navigation : true,	responsive: true, responsiveRefreshRate : 200,	slideSpeed : 200,	
- 		paginationSpeed : 200,	rewindSpeed : 500,	items:3,  itemsTablet: [768,2], autoPlay : true,
-		itemsMobile : [479,1], mouseDrag:false
-	});
+    /*Main Menu Button */
+    $('.main_menu_btn').on("click", function (e) {
+      $(this).toggleClass('main_menu_btn_open');
+      $('.main_menu_block').toggleClass('main_menu_block_open').fadeToggle();
+      $('.main_menu_block').find('.menu_wrapper').toggleClass('active');
+      $('header .anim').toggleClass('active');
+      e.preventDefault();
+    });
 
-	/*Registry Carousel */
-	$(".registry_wrapper").owlCarousel({
- 		navigation : true, responsive: true, responsiveRefreshRate : 200, slideSpeed : 200, paginationSpeed : 200,
-		rewindSpeed : 500,	stopOnHover : true, autoHeight : true, items:3, mouseDrag:false, autoPlay : true
-	});
+    /* Section Background */
+    $('section, .parallax').each(function () {
+      var image = $(this).attr('data-image');
+      if (image) {
+        $(this).css('background-image', 'url(' + image + ')');
+      }
+    });
 
-	/*The Crew Carousel*/
-	$(".guest_wrapper").owlCarousel({
- 		navigation : true, responsive: true, responsiveRefreshRate : 200, slideSpeed : 200,	paginationSpeed : 200,
-		rewindSpeed : 500, stopOnHover : true, autoHeight : true, items:4, mouseDrag:false, autoPlay : true
-	});	
+    /*ColorBox*/
+    if ($(window).width() >= 760) {
+      $(".youtube").colorbox({iframe: true, innerWidth: 640, innerHeight: 390});
+    } else {
+      $(".youtube").colorbox({iframe: true, innerWidth: 320, innerHeight: 240});
+    }
+    $(window).resize(function () {
+      if ($(window).width() >= 760) {
+        $(".youtube").colorbox({iframe: true, innerWidth: 640, innerHeight: 390});
+      } else {
+        $(".youtube").colorbox({iframe: true, innerWidth: 320, innerHeight: 240});
+      }
+    });
 
-	/*Slider Carousel*/
-	$(".slider").owlCarousel({
- 		navigation : true, responsive: true, responsiveRefreshRate : 200, slideSpeed : 200,	paginationSpeed : 200,
-		rewindSpeed : 500, stopOnHover : false, autoHeight : true, singleItem:true, mouseDrag:false, autoPlay : true, transitionStyle : "fade"
-	});	
+    /*Scroll Effect*/
+    $('.intro_down, .go').on("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+      }, 1000);
+      e.preventDefault();
+    });
 
-	/* Top Menu Click to Section */
-	$('.sub_menu').find('a').on("click", function(e){
-		$('.sub_menu').find('a').removeClass('active');
-		$(this).addClass('active');
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top +1
-		}, 1000);
-		$(".main_menu_btn").trigger('click');
-		e.preventDefault();
-	});
-	
-	
-	/*FireFly in Intro*/
-	$.firefly({
-		color: '#fff', minPixel: 1, maxPixel: 3, total : 55, on: '.into_firefly'
-	});
-	
-	/* Refresh ScrollR */
-	s.refresh($(".guest_wrapper, .our_story"));
+    /*Show/Hide Photo in When&Where Block*/
+    $('.photocamera span').on("click", function (e) {
+      $(this).parents('section').find('.opacity').toggleClass('fade');
+      $(this).parents('section').find('.over').fadeToggle();
+      e.preventDefault();
+    });
 
-})(jQuery);
+    /*Player*/
+    $('.music').on("click", function (e) {
+      $('.player').fadeToggle();
+      e.preventDefault();
+    });
+
+    /*CountDown*/
+    $('.married_coundown').countdown({until: new Date("March 1, 2017 11:00:00")});
+
+    /*OWL Carousel in Our Story*/
+    $(".story_wrapper").owlCarousel({
+      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200,
+      paginationSpeed: 200, rewindSpeed: 500, items: 3, itemsTablet: [768, 1], autoPlay: true,
+      itemsMobile: [479, 1], itemsDesktopSmall: [980, 1], itemsDesktop: [1500, 2], mouseDrag: false
+    });
+
+    /*Gallery Carousel */
+    $(".gallery_wrapper").owlCarousel({
+      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200,
+      paginationSpeed: 200, rewindSpeed: 500, items: 3, itemsTablet: [768, 2], autoPlay: true,
+      itemsMobile: [479, 1], mouseDrag: false
+    });
+
+    /*Registry Carousel */
+    $(".registry_wrapper").owlCarousel({
+      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200, paginationSpeed: 200,
+      rewindSpeed: 500, stopOnHover: true, autoHeight: true, items: 3, mouseDrag: false, autoPlay: true
+    });
+
+    /*The Crew Carousel*/
+    $(".guest_wrapper").owlCarousel({
+      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200, paginationSpeed: 200,
+      rewindSpeed: 500, stopOnHover: true, autoHeight: true, items: 4, mouseDrag: false, autoPlay: true
+    });
+
+    /*Slider Carousel*/
+    $(".slider").owlCarousel({
+      navigation: true,
+      responsive: true,
+      responsiveRefreshRate: 200,
+      slideSpeed: 200,
+      paginationSpeed: 200,
+      rewindSpeed: 500,
+      stopOnHover: false,
+      autoHeight: true,
+      singleItem: true,
+      mouseDrag: false,
+      autoPlay: true,
+      transitionStyle: "fade"
+    });
+
+    /* Top Menu Click to Section */
+    $('.sub_menu').find('a').on("click", function (e) {
+      $('.sub_menu').find('a').removeClass('active');
+      $(this).addClass('active');
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top + 1
+      }, 1000);
+      $(".main_menu_btn").trigger('click');
+      e.preventDefault();
+    });
+
+    /*FireFly in Intro*/
+    $.firefly({
+      color: '#fff', minPixel: 1, maxPixel: 3, total: 55, on: '.into_firefly'
+    });
+
+    /* Refresh ScrollR */
+    s.refresh($(".guest_wrapper, .our_story"));
+
+    function initializeMap () {
+      var myLocation = new google.maps.LatLng(13.0429876, 77.6145404);
+
+      var mapOptions = {
+        zoom: 12,
+        center: myLocation,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      var map = new google.maps.Map(document.getElementById("gmap_canvas"), mapOptions);
+
+      var marker = new google.maps.Marker({map: map, position: myLocation});
+
+      var address = "<strong>Naman Maheshwari</strong>" +
+                    "<br/>No. 91/4, 102/3," +
+                    "<br/>Opp BEL Corporate Office," +
+                    "<br/>Hebbal Flyover," +
+                    "<br/>Veerannapalya Rd," +
+                    "<br/>DadaMastan Layout," +
+                    "<br/>Manayata Tech Park, Nagavara" +
+                    "<br>Bengaluru, Karnataka - 560024 <br>";
+
+      var infowindow = new google.maps.InfoWindow({content: address, maxWidth: 400});
+
+      infowindow.open(map, marker);
+    }
+
+    google.maps.event.addDomListener(window, "load", initializeMap);
+
+
+  });
+}(jQuery));
